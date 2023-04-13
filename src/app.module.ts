11138d4c629e2c4e399b063c08c35de2b6f4e46a
robin <robin@gocrazy.cn>
2023-04-13 14:41:59 +0800

    add configuration module into project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGptModule } from './chat-gpt/chat-gpt.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [ChatGptModule],
+  imports: [ChatGptModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
